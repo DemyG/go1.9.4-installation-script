3b85194ed08892be0fcc2bfcbcd182ed7c2c1769
DemyG <dimitra.giantsidi@gmail.com>
2018-03-26 20:37:34 +0000

    use source for executing the script

--- install_go1.4.9.sh
+++ install_go1.4.9.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
+## execute with `source install_go1.4.9.sh` so
+## that env variables be set in current shell and 
+## not in child-sub-shells
+
+## `export` exports the variable assignment to sub-shells, i.e. 
+## shells which are started as child processes of the shell 
+## containing the export directive. Your command-line environment 
+## is the parent of the script's shell, so it does not see the 
+## variable assignment.
+
+## You can use the `source` bash command to execute the script 
+## commands in the current shell environment and achieve what you want, e.g.
+
 wget https://dl.google.com/go/go1.9.4.linux-amd64.tar.gz
 
 sudo tar -xvf go1.9.4.linux-amd64.tar.gz
-sudo mv go go1.9.4
+sudo mv go /home/ubuntu/go1.9.4
 
-mkdir go
+mkdir /home/ubuntu/go
 
 export GOROOT=/home/ubuntu/go1.9.4
 export GOPATH=/home/ubuntu/go
